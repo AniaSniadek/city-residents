806c7c8d6f52dd58868d9bea9f6762f139c52fef
Speedy09 <sniadek97@gmail.com>
2020-04-17 16:25:37 +0200

    addPerson to list

--- src/Citizens.java
+++ src/Citizens.java
@@ -1,2 +1,17 @@
+import java.util.Vector;
+
 public class Citizens {
+
+    static Vector<Person> peopleList = new Vector<Person>();
+
+    public void addPerson(Person person){
+
+        for(Person p : peopleList){
+            if(p.getPesel().equals(person.getPesel())){
+                System.out.println("Uzytkownik o podanym peselu juz istnieje");
+                return;
+            }
+            peopleList.add(person);
+        }
+    }
 }
