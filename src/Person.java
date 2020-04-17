52856ce64b0b3e5fa6b8619888ece82ff1c64fcc
Speedy09 <sniadek97@gmail.com>
2020-04-17 15:51:14 +0200

    start Person

---
+++ src/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+
+    public String city;
+    public String name;
+    public String surname;
+    public String pesel;
+
+    public void writePerson(){
+
+    }
+
+    public boolean checkPesel() {
+
+    }
+}
