bc974f08144068e3824cf8f325607a75690d029b
Speedy09 <sniadek97@gmail.com>
2020-04-17 16:31:50 +0200

    details as string

--- src/Person.java
+++ src/Person.java
@@ -15,18 +15,18 @@ public class Person {
 
     public boolean checkPesel() {
 
-        if(getPesel().length() != 11)
+        if(this.getPesel().length() != 11)
             return false;
 
-        int sum = (9 * ((getPesel().charAt(0) - '0') + (getPesel().charAt(4) - '0') +
-                (getPesel().charAt(8)) - '0')) + (7 * ((getPesel().charAt(1) - '0') +
-                (getPesel().charAt(5) - '0') + (getPesel().charAt(9) - '0'))) +
-                (3 * ((getPesel().charAt(2) - '0') + (getPesel().charAt(6) - '0'))) +
-                (getPesel().charAt(3) - '0') + (getPesel().charAt(7) - '0');
+        int sum = (9 * ((this.getPesel().charAt(0) - '0') + (this.getPesel().charAt(4) - '0') +
+                (this.getPesel().charAt(8)) - '0')) + (7 * ((this.getPesel().charAt(1) - '0') +
+                (this.getPesel().charAt(5) - '0') + (this.getPesel().charAt(9) - '0'))) +
+                (3 * ((this.getPesel().charAt(2) - '0') + (this.getPesel().charAt(6) - '0'))) +
+                (this.getPesel().charAt(3) - '0') + (this.getPesel().charAt(7) - '0');
         System.out.println(sum);
         int score = sum % 10;
 
-        if(score == (getPesel().charAt(10) - '0'))
+        if(score == (this.getPesel().charAt(10) - '0'))
             return true;
 
         return false;
@@ -46,5 +46,11 @@ public class Person {
 
     public String getPesel() {
         return pesel;
+    }
+
+    public String getDetailsAsString(){
+        String text = this.getCity() + " " + this.getName() + " " +
+                    this.getSurname() + " " + this.getPesel();
+        return text;
     }
 }
